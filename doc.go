8200f0001b5a5be9c75f4c2f013f3bc936e34231
Sean <sean@beamery.com>
2018-07-28 23:03:43 +0100

    Adding further doc support

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Kubeaware is built in order to bring order
+// for those applications who don't have cloud native support out of the box.
+// This becomes important once companies start porting their legacy systems
+// onto platforms like Kubernetes
+package main
---
+++ types/doc.go
@@ -0,0 +1,2 @@
+// Package Types is a way to rationalise the various types used within this project.
+package types
